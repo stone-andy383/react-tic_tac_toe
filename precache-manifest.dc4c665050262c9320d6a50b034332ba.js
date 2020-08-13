a3ddf149c8c5c747c93d30d61326cfda61fc7b40
stone-andy383 <Andy@Leanas-MacBook-Pro-2.local>
2020-08-12 20:02:18 -0500

    Updates

---
+++ precache-manifest.dc4c665050262c9320d6a50b034332ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32917625a7f7f92dbe3726507eb0cb5a",
+    "url": "/react-tic_tac_toe/index.html"
+  },
+  {
+    "revision": "a5e8f03639ee83815f24",
+    "url": "/react-tic_tac_toe/static/css/main.0ebde3ef.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/react-tic_tac_toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic_tac_toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5e8f03639ee83815f24",
+    "url": "/react-tic_tac_toe/static/js/main.11bfdda6.chunk.js"
+  },
+  {
+    "revision": "02dceef944e68ec410f1",
+    "url": "/react-tic_tac_toe/static/js/runtime-main.0893a6f6.js"
+  }
+]);
